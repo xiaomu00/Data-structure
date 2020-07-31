e1de2012970476076ff5689214aea46336e7b429
xiaomu00 <xiaomu_Y@163.com>
2020-07-31 09:01:42 +0800

    顺序表

--- Data_structure/Data-structure/seqlist.h
+++ Data_structure/Data-structure/seqlist.h
@@ -3,293 +3,312 @@
 
 #include"public.h"
 
+#define SEQLIST_DEFAULT_SIZE 8
 
-//定义链表节点
-typedef struct ListNode
+//定义顺序表结构
+typedef struct SeqList
 {
-	ElemType data;
-	struct ListNode *next;
-}ListNode;
-typedef ListNode* List;
+	ElemType *base;
+	size_t    capacity;
+	size_t    size;
+}SeqList;
+
+///////////////////////////////////////////////////////////
+//声明函数接口
+static bool _Inc(SeqList *pst); //增加成功返回 true, 失败返回false
+
+void SeqListInit(SeqList *pst);
+bool IsFull(SeqList *pst);
+bool IsEmpty(SeqList *pst);
+void SeqListPushBack(SeqList *pst, ElemType x);
+void SeqListPushFront(SeqList *pst, ElemType x);
+void SeqListPopBack(SeqList *pst);
+void SeqListPopFront(SeqList *pst);
+
+void SeqListInsertByPos(SeqList *pst, int pos, ElemType x);
+void SeqListInsertByVal(SeqList *pst, ElemType x);
+void SeqListDeleteByPos(SeqList *pst, int pos);
+void SeqListDeleteByVal(SeqList *pst, ElemType key);
+void SeqListRemoveAll(SeqList *pst, ElemType key);
+void SeqListSort(SeqList *pst);
+int SeqListFind(SeqList *pst, ElemType key);
+void SeqListReverse(SeqList *pst);
+
+size_t SeqListLength(SeqList *pst);
+size_t SeqListCapacity(SeqList *pst);
+void SeqListClear(SeqList *pst);
+void SeqListShow(SeqList *pst);
+void SeqListDestroy(SeqList *pst);
 
 
-void ListCreate_Tail(List *plist);//尾插法创建单链表
-void ListCreate_Head(List *plist);//头插法创建单链表
-void Listshow(List plist);//显示单链表
-
-//
-////不带头节点
-////初始化
-//void List_Init(List *plist)//相当于ListNode **plist
-//{*plist = NULL;}
-////尾插法创建单链表
-//void ListCreate_Tail(List *plist)
-//{
-//	*plist = (ListNode *)malloc(sizeof(ListNode));
-//	assert(*plist!=NULL);
-//	(*plist)->data = 1;
-//	(*plist)->next = NULL;
-//	ListNode *p = *plist;
-//	for (int i = 2; i <= 10; i++)
-//	{
-//		ListNode *s = (ListNode *)malloc(sizeof(ListNode));
-//		assert(s!= NULL);
-//		s->data = i;
-//		s->next = NULL;
-//		p->next = s;
-//		p = s;
-//	}
-//}
-////头插法创建单链表
-//void ListCreate_Head(List *plist)
-//{
-//	*plist = (ListNode *)malloc(sizeof(ListNode));
-//	assert(*plist != NULL);
-//	(*plist)->data = 1;
-//	(*plist)->next = NULL;
-//	for (int i = 2; i <= 10; i++)
-//	{
-//		ListNode *s = (ListNode *)malloc(sizeof(ListNode));
-//		assert(s != NULL);
-//		s->data =i;
-//		s->next = (*plist);
-//		(*plist) = s;
-//	}
-//}
-////显示单链表
-//void  Listshow(List plist)
-//{
-//	ListNode *p = plist;
-//	while (p!= NULL)
-//	{
-//		printf("%d->",p->data);
-//		p = p->next;
-//	}
-//	printf("Over\n");
-//}
-
-
-//带头节点
-//初始化
-void List_Init(List *plist)//相当于ListNode **plist
+//////////////////////////////////////////////////////////
+//函数接口实现
+static bool _Inc(SeqList *pst)
 {
-	*plist = (ListNode *)malloc(sizeof(ListNode));
-	assert(*plist != NULL);
-	(*plist)->next = NULL;
+	ElemType *new_base = (ElemType*)realloc(pst->base, sizeof(ElemType)*pst->capacity * 2);
+	if (new_base == NULL)
+		return false;
+	pst->base = new_base;
+	pst->capacity *= 2;
+	return true;
 }
-void ListCreate_Tail(List *plist);//尾插法创建单链表
-void ListCreate_Head(List *plist);//头插法创建单链表
-void Listshow(List plist);//显示单链表
 
-
-//尾插法创建单链表
-void ListCreate_Tail(List *plist)
+void SeqListInit(SeqList *pst)
 {
-	ListNode *p = *plist;
-	for (int i = 1; i <= 10; i++)
+	assert(pst != NULL);
+	pst->base = (ElemType*)malloc(sizeof(ElemType)* SEQLIST_DEFAULT_SIZE);
+	assert(pst->base != NULL);
+	memset(pst->base, 0, sizeof(ElemType)* SEQLIST_DEFAULT_SIZE);
+	pst->capacity = SEQLIST_DEFAULT_SIZE;
+	pst->size = 0;
+}
+
+bool IsFull(SeqList *pst)
+{
+	assert(pst != NULL); return pst->size >= pst->capacity;
+}
+bool IsEmpty(SeqList *pst)
+{
+	assert(pst != NULL); return pst->size == 0;
+}
+
+void SeqListPushBack(SeqList *pst, ElemType x)
+{
+	assert(pst != NULL);
+
+	if (IsFull(pst) && !_Inc(pst)) //短路求值
 	{
-		ListNode *s = (ListNode *)malloc(sizeof(ListNode));
-		assert(s != NULL);
-		s->data = i;
-		s->next = NULL;
-		p->next = s;
-		p = s;
-	}
-}
-//头插法创建单链表
-void ListCreate_Head(List *plist)
-{
-	for (int i = 1; i <= 10; i++)
-	{
-		ListNode *s = (ListNode *)malloc(sizeof(ListNode));
-		assert(s != NULL);
-		s->data = i;
-
-		s->next = (*plist)->next;
-		(*plist)->next = s;
-	}
-}
-
-//显示单链表
-void  Listshow(List plist)
-{
-	ListNode *p = plist;
-	p = p->next;
-	while (p != NULL)
-	{
-		printf("%d->", p->data);
-		p = p->next;
-	}
-	printf("Over\n");
-}
-
-
-
-typedef struct SListNode
-{
-	ElemType data;
-	struct SListNode *next;
-}SListNode;
-typedef struct slist
-{
-	SListNode *head;
-}slist;
-
-////////////////////////////////////////////////////////////////////
-//单链表的函数接口声明
-static SListNode* _BuyNode(ElemType x);//开辟一个节点空间并赋值
-void listInit(slist *pst);//单链表序表初始化函数声明
-void slistPushBackPushBack(slist *plist, ElemType x);//尾部插入
-void slistPushFront(slist *plist, ElemType x);//头部插入
-void slistPopBack(slist *plist);//尾部删除
-void slistPopFront(slist *plist);//头部删除
-int slistlength(slist *plist);//求表长
-void slistDestroy(slist *plist);//摧毁
-void slistShow(slist *plist);//显示
-
-
-
-
-////////////////////////////////////////////////////////////////////
-//单链表的函数接口实现
-
-//开辟一个节点空间并赋值
-static SListNode* _BuyNode(ElemType x)
-{
-	SListNode *s = (SListNode*)malloc(sizeof(SListNode));
-	assert(s != NULL);
-	s->data = x;
-	s->next = NULL;
-	return s;
-}
-//单链表初始化函数声明
-void listInit(slist *plist)
-{
-	plist->head = NULL;
-}
-//尾部插入 时间复杂度为O（n）
-void slistPushBackPushBack(slist *plist, ElemType x)
-{
-	assert(plist != NULL);
-	SListNode *s = _BuyNode(x);
-	if (plist->head == NULL)
-	{
-		plist->head = s;
+		//空间满了并扩容不成功
+		printf("顺序表空间已满,不能插入数据 %d\n", x);
 		return;
 	}
-	SListNode *p = plist->head;
-	while (p->next != NULL)
-	{
-		p = p->next;
-	}
-	p->next = s;
+
+	pst->base[pst->size++] = x;
 }
 
-//头部插入  时间复杂度为O（1）
-void slistPushFront(slist *plist, ElemType x)
+void SeqListPushFront(SeqList *pst, ElemType x)
 {
-	assert(plist != NULL);
-	SListNode *s = _BuyNode(x);
-	s->next = plist->head;
-	plist->head = s;
-}
-
-
-//尾部删除  
-void slistPopBack(slist *plist)
-{
-	assert(plist != NULL);
-	if (plist->head == NULL)
+	assert(pst != NULL);
+	if (IsFull(pst) && !_Inc(pst))
 	{
-		printf("链表已空！\n");
+		printf("顺序表空间已满,不能插入数据 %d\n", x);
 		return;
 	}
-	SListNode *p = plist->head;
-	if (p->next == NULL)//说明只有一个节点
+	for (size_t pos = pst->size; pos>0; --pos)
+		pst->base[pos] = pst->base[pos - 1];
+	pst->base[0] = x;
+	pst->size++;
+}
+
+void SeqListPopBack(SeqList *pst)
+{
+	assert(pst != NULL);
+	if (IsEmpty(pst))
 	{
-		free(p);
-		plist->head = NULL;
-		printf("删除成功!\n");
+		printf("顺序表已空,不能尾部删除元素.\n");
 		return;
 	}
-	else
-	while (p->next->next != NULL)
-	{
-		p = p->next;
-	}
-	free(p->next);
-	p->next = NULL;
-	printf("删除成功!\n");
+	pst->size--;
 }
-
-//头部删除   
-void slistPopFront(slist *plist)
+void SeqListPopFront(SeqList *pst)
 {
-	assert(plist != NULL);
-	if (plist->head == NULL)
+	assert(pst != NULL);
+	if (IsEmpty(pst))
 	{
-		printf("链表已空！\n");
+		printf("顺序表已空,不能头部删除元素.\n");
 		return;
 	}
-	SListNode *p = plist->head;
-	plist->head = p->next;
-	free(p);
-	printf("删除成功!\n");
+
+	for (int i = 0; i<pst->size; ++i)
+		pst->base[i] = pst->base[i + 1];
+	pst->size--;
 }
 
-//求表长
-int slistlength(slist *plist)
+void SeqListInsertByPos(SeqList *pst, int pos, ElemType x)
 {
-
-	SListNode *p;
-	assert(plist != NULL);
-	if (plist->head == NULL)
-		return 0;
-	p = plist->head;
-	int len = 0;
-	while (p != NULL)
+	assert(pst != NULL);
+	if (pos<0 || pos>pst->size)
 	{
-		len++;
-		p = p->next;
-	}
-	return len;
-}
-
-
-//摧毁
-void slistDestroy(slist *plist)
-{
-	if (plist->head == NULL)
-	{
-		printf("链表已空！\n");
+		printf("要插入数据的位置非法，%d 不能插入.\n", x);
 		return;
 	}
-	while (plist->head != NULL)
+	if (IsFull(pst) && !_Inc(pst))
 	{
-		SListNode *p = plist->head;
-		plist->head = p->next;
-		free(p);
-	}
-	printf("摧毁成功！\n");
-}
-
-//显示
-void slistShow(slist *plist)
-{
-	if (plist->head == NULL)
-	{
-		printf("链表已空！\n");
+		printf("顺序表空间已满,不能按位置插入数据 %d\n", x);
 		return;
 	}
-	SListNode *p = plist->head;
-	while (p != NULL)
-	{
-		printf("%d -> ", p->data);
-		p = p->next;
-	}
-	printf("Over\n");
+	for (int i = pst->size; i>pos; --i)
+		pst->base[i] = pst->base[i - 1];
+	pst->base[pos] = x;
+	pst->size++;
 }
 
+void SeqListDeleteByPos(SeqList *pst, int pos)
+{
+	assert(pst != NULL);
+	if (IsEmpty(pst))
+	{
+		printf("顺序表已空,不能按位置删除元素.\n");
+		return;
+	}
+	if (pos<0 || pos >= pst->size)
+	{
+		printf("要删除数据的位置非法，不能按位置删除数据.\n");
+		return;
+	}
+	for (int i = pos; i<pst->size; ++i)
+		pst->base[i] = pst->base[i + 1];
+	pst->size--;
+}
+
+void SeqListDeleteByVal(SeqList *pst, ElemType key)
+{
+	assert(pst != NULL);
+	if (IsEmpty(pst))
+	{
+		printf("顺序表已空,不能按值删除元素.\n");
+		return;
+	}
+	int pos = SeqListFind(pst, key);
+	if (pos == -1)
+	{
+		printf("要删除的数据不存在,不能删除.\n");
+		return;
+	}
+	SeqListDeleteByPos(pst, pos);
+}
+
+void SeqListInsertByVal(SeqList *pst, ElemType x)
+{
+	//从后往前比较
+	assert(pst != NULL);
+	if (IsFull(pst) && !_Inc(pst))
+	{
+		printf("顺序表空间已满,不能按值插入数据 %d\n", x);
+		return;
+	}
+
+	int end = pst->size;
+	while (end>0 && x<pst->base[end - 1])
+	{
+		pst->base[end] = pst->base[end - 1];
+		end--;
+	}
+	pst->base[end] = x;
+	pst->size++;
+}
+
+void SeqListSort(SeqList *pst)
+{
+	assert(pst != NULL);
+	if (pst->size <= 1)
+		return;
+	for (int i = 0; i<pst->size - 1; ++i)
+	{
+		for (int j = 0; j<pst->size - i - 1; ++j)
+		{
+			if (pst->base[j] > pst->base[j + 1])
+			{
+				ElemType tmp = pst->base[j];
+				pst->base[j] = pst->base[j + 1];
+				pst->base[j + 1] = tmp;
+			}
+		}
+	}
+}
+
+int SeqListFind(SeqList *pst, ElemType key)
+{
+	assert(pst != NULL);
+	for (int i = 0; i<pst->size; ++i)
+	{
+		if (key == pst->base[i])
+			return i;
+	}
+	return -1;
+}
+
+void SeqListReverse(SeqList *pst)
+{
+	assert(pst != NULL);
+	if (pst->size < 2)
+		return;
+
+	int left = 0;
+	int right = pst->size - 1;
+	while (left < right)
+	{
+		Swap(&pst->base[left], &pst->base[right]);
+		left++;
+		right--;
+	}
+}
+
+size_t SeqListLength(SeqList *pst)
+{
+	assert(pst != NULL);
+	return pst->size;
+}
+
+size_t SeqListCapacity(SeqList *pst)
+{
+	assert(pst != NULL);
+	return pst->capacity;
+}
+
+void SeqListClear(SeqList *pst)
+{
+	assert(pst != NULL);
+	pst->size = 0;
+}
+
+void SeqListShow(SeqList *pst)
+{
+	assert(pst != NULL);
+	for (size_t i = 0; i<pst->size; ++i)
+		printf("%d ", pst->base[i]);
+	printf("\n");
+}
+
+void SeqListDestroy(SeqList *pst)
+{
+	assert(pst != NULL);
+	if (pst->base)
+		free(pst->base);
+	pst->base = NULL;
+	pst->capacity = pst->size = 0;
+}
+
+void SeqListRemoveAll(SeqList *pst, ElemType key)
+{
+	//
+}
+
+/*
+void SeqListInsertByVal(SeqList *pst, ElemType x)
+{
+//从前往后比较
+assert(pst != NULL);
+if(IsFull(pst))
+{
+printf("顺序表空间已满,不能按值插入数据 %d\n", x);
+return;
+}
+int pos = 0;
+int i;
+for(i=0; i<pst->size; ++i)
+{
+if(x < pst->base[i])
+{
+pos = i;
+break;
+}
+}
+if(i >= pst->size)
+pos = i;
+SeqListInsertByPos(pst, pos, x);
+}
+*/
 
 
 #endif /*_SEQLIST_H_*/
